491572f1ed3ebfbe1fdc6a27feb094884c502096
imgntn159 <imgtn159@gmail.com>
2016-03-30 13:22:34 +0000

    Updating part2, to revert to old behavior change script 2 to script

---
+++ parse.py
@@ -0,0 +1,43 @@
+import csv
+fileReader = csv.reader(open('original.csv'))
+data = []
+for row in fileReader:
+    if row[1] == 'Washington':
+        break
+    data.append(row[1:-2])
+data = data [2:]
+
+dataV2 = []
+doubles = []
+for row in data:
+    if row[2] == 'D&R':
+        dataV2.append(row)
+    else:
+        if row[0] in [ item[0] for item in data[(data.index(row)+1):] ] :
+            doubles.append(row)
+        elif row[0] in [ item[0] for item in doubles ] :
+            temp = []
+            final = row
+            for i in doubles:
+                if i[0] == row[0]:
+                    temp = i
+                    break
+            if temp[2] == 'D':
+                final[6] = temp[6]
+            else:
+                final[7] = temp[7]
+            dataV2.append(final)
+            
+
+for row in dataV2:
+    del row[2]
+    #3, 4, 5, 6, 7, 8
+    for i in range (6):
+        row[i+3] = row[i+3].replace(",", "")
+        row[i+3] = row[i+3].replace(" (est.)", "")
+    print row
+
+dataV2.insert(0, ['State', 'Election Type', 'VEP Ballots Counted', 'VEP', 'VAP', 'Democrat', 'Republican', 'Minor', 'Total Ballots'])
+with open("data.csv", "wb") as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar=" ")
+    writer.writerows(dataV2)
